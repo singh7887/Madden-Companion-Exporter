b509a22be8979570f1d6752915f521531593cd4e
singh7887 <67026777+singh7887@users.noreply.github.com>
2020-06-16 16:49:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
 // const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
+cfm-what-s-up-gamers-firebase-adminsdk-a69y4-56bd998d60.json
 // TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+ admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: "https://cfm-what-s-up-gamers.firebaseio.com/"
+ });
 
 app.set('port', (process.env.PORT || 3001));
 
